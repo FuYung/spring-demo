748d7e83d1bde742cc0280190f308c37b17376c1
vic <fuyong1208@163.com>
2016-04-26 17:51:32 +0800

    jobParameters

--- spring-batch-demo/src/test/java/cn/dreamfy/example/ch01/JobLaunchTest.java
+++ spring-batch-demo/src/test/java/cn/dreamfy/example/ch01/JobLaunchTest.java
@@ -29,7 +29,10 @@ public class JobLaunchTest {
 
 	@Test
 	public void importProducts() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
-		JobExecution jobExecution = jobLauncher.run(job, new JobParametersBuilder().addString("date", "2016-01-03").toJobParameters());
+		JobExecution jobExecution = jobLauncher.run(job, new JobParametersBuilder()
+				.addString("inputResource", "classpath:ch01/data/credit-card-bill-201604.csv")
+				.addString("date", "2016-01-03")
+				.toJobParameters());
 		System.out.println("status : " + jobExecution.toString());
 	}
 }
